ed73b175f345747a204c1b86c0ea53d08e27ad13
David Runger <davidjrunger@gmail.com>
2021-02-01 18:05:28 -0800

    Allow for managing releases via RubyGems (in addition to GitHub)

---
+++ lib/release_assistant/config_file_reader.rb
@@ -0,0 +1,25 @@
+# frozen_string_literal: true
+
+class ReleaseAssistant::ConfigFileReader
+  extend Memoist
+
+  memoize \
+  def options_hash
+    if config_file_exists?
+      YAML.load_file(config_file_path).symbolize_keys
+    else
+      {}
+    end
+  end
+
+  private
+
+  memoize \
+  def config_file_path
+    "#{ENV['PWD']}/.release_assistant.yml"
+  end
+
+  def config_file_exists?
+    File.exist?(config_file_path)
+  end
+end
--- lib/release_assistant/version.rb
+++ lib/release_assistant/version.rb
@@ -2,6 +2,6 @@
 
 # rubocop:disable Style/StaticClass
 class ReleaseAssistant
-  VERSION = '0.2.1.alpha'
+  VERSION = '0.3.0.alpha'
 end
 # rubocop:enable Style/StaticClass
