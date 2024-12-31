0903aa94bfb7096e4f47afe2db3cf5c7e0effbbd
David Runger <davidjrunger@gmail.com>
2024-12-31 11:52:02 -0600

    Prepare to release v0.14.0

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,4 +1,4 @@
-## Unreleased
+## v0.14.0 (2024-12-31)
 - Ensure we are on the primary branch when releasing
 
 ## v0.13.0 (2024-12-10)
--- lib/runger_release_assistant/version.rb
+++ lib/runger_release_assistant/version.rb
@@ -2,6 +2,6 @@
 
 # rubocop:disable Style/StaticClass
 class RungerReleaseAssistant
-  VERSION = '0.13.1.alpha'
+  VERSION = '0.14.0'
 end
 # rubocop:enable Style/StaticClass
